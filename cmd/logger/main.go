5456366430df9d47630ce5a707776578b8235a14
Frank Francione <frankiefrancione@gmail.com>
2025-12-26 09:02:28 -0600

    added pubsub client

--- cmd/logger/main.go
+++ cmd/logger/main.go
@@ -1,12 +1,42 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 
 	"go-log-aggregator/internal/producer"
+	mypubsub "go-log-aggregator/internal/pubsub"
 )
 
 func main() {
 	fmt.Println("This new project will be awesome!!!")
+	/* 
+		Creates Pub / Sub client
+		and connects to my Google Account
+	*/
+	ctx := context.Background()
+
+	client, err := mypubsub.NewClient(ctx, "go-log-aggregator")
+	if (err != nil) {
+		log.Fatal("Failed to create pub/sub client: ", err)
+	}
+	defer client.Close()
+	/* 
+		Produces a set ammount of logs 
+		and Uploads to Pub / Sub
+	*/
 	producer.ProduceLog()
+
+	/* 
+		Receives logs from Pub / Sub
+	*/
+	// aggregator here
+
+	/* 
+		Parses logs
+	*/
+	// parser here
+
+	// Make templates here?
 }
--- internal/pubsub/client.go
+++ internal/pubsub/client.go
@@ -1,1 +1,15 @@
-package pubsub
+package mypubsub
+
+import (
+	"context"
+
+	"cloud.google.com/go/pubsub"
+)
+
+func NewClient(ctx context.Context, projectID string) (*pubsub.Client, error) {
+	client, err := pubsub.NewClient(ctx, projectID)
+	if (err != nil) {
+		return nil, err;
+	}
+	return client, nil;
+}
